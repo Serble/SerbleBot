85093993f67dd2873ebff09f12f3947059cf8a44
copokbl <zaneharrison2006@icloud.com>
2022-07-12 16:27:50 +1000

    Made clear command oh so much better

--- Commands/ExecutionFunctions/ClearCommand.cs
+++ Commands/ExecutionFunctions/ClearCommand.cs
@@ -13,12 +13,31 @@ public class ClearCommand : ICommandExecutionHandler {
         if (messageCount > 1000)
             await cmd.RespondAsync("You cannot delete more than 1000 messages at once.");
 
+        int messagesCount;
         try {
             IEnumerable<IMessage> messages = 
-                (await cmd.Channel.GetMessagesAsync(messageCount + 1).FlattenAsync()).Skip(1);
-            foreach (var message in messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14)) {
-                await message.DeleteAsync();
+                (await cmd.Channel
+                    .GetMessagesAsync(messageCount + 1)
+                    .FlattenAsync())
+                    .Skip(1)
+                    .Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14);
+            IEnumerable<IMessage> enumeratedMessages = messages as IMessage[] ?? messages.ToArray();
+            messagesCount = enumeratedMessages.Count();
+            
+            if (cmd.Channel is not ITextChannel textChannel) {
+                // Create embed[]
+                Embed[] embeds = new Embed[1];
+                embeds[0] = new EmbedBuilder()
+                    .WithTitle("Usage")
+                    .WithDescription("You can only clear messages in non DM text channels.")
+                    .WithColor(Color.Red)
+                    .Build();
+                await cmd.ModifyOriginalResponseAsync(msg => msg.Embeds = new Optional<Embed[]>(embeds));
+                return;
             }
+            
+            // Mass delete
+            await textChannel.DeleteMessagesAsync(enumeratedMessages);
         }
         catch (HttpException) {
             await cmd.ModifyOriginalResponseAsync(msg => msg.Content = 
@@ -26,6 +45,7 @@ public class ClearCommand : ICommandExecutionHandler {
             return;
         }
 
-        await cmd.ModifyOriginalResponseAsync(msg => msg.Content = $"Deleted {messageCount} messages.");
+        await cmd.ModifyOriginalResponseAsync(msg => 
+            msg.Content = $"Deleted {messagesCount} message{(messagesCount == 1 ? "" : "s")}.");
     }
 }
